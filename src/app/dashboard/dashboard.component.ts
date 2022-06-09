06c19bdf053961e364fb8f2c6155bd601d12cf36
varunchinna222 <varunarjun222@gmail.com>
2022-06-09 10:33:53 +0530

    dropdown changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  logout: boolean = false
+  drop: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onuser(){
+    this.logout = !this.logout
+  }
+
+  onhead(){
+    this.drop = !this.drop
+  }
+
+  onhead1(){
+    this.drop = !this.drop
+  }
+ 
+  onhead2(){
+    this.drop = !this.drop
+  }
+ 
+
+  onhead3(){
+    this.drop = !this.drop
+  }
+  onuser4(){
+    this.logout = !this.logout
+  }
+
+  onhead4(){
+    this.drop = !this.drop
+  }
+  
+  onhead5(){
+    this.drop = !this.drop
+  }
+  
+  onhead6(){
+    this.drop = !this.drop
+  }
+  onhead7(){
+    this.drop = !this.drop
+  }
+  onhead8(){
+    this.drop = !this.drop
+  }
+  onhead9(){
+    this.drop = !this.drop
+  }
+ 
+
 
 }
